4c9f3c1d20a55a625a529347051357f462cec448
mbroersen <m.broersen@socialblue.com>
2019-05-25 16:07:46 +0200

    another try

--- routes/web.php
+++ routes/web.php
@@ -1,11 +1,11 @@
 <?php
 
 use Illuminate\Support\Facades\Cache;
-use Illuminate\Support\Facades\Route;
+
 
 Route::get('/', view('laravel-query-adviser::index'));
 
 
-//Route::get('/query/list', function () {
-//    return Cache::get(config('laravel-query-adviser.cache.key'), []);
-//});
+Route::get('/query/list', function () {
+    return Cache::get(config('laravel-query-adviser.cache.key'), []);
+});
