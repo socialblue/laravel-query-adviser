d4a1ff0cada49efad4042c2e6e80741eff27660e
mbroersen <m.broersen@socialblue.com>
2019-05-25 15:54:25 +0200

    another try

--- routes/web.php
+++ routes/web.php
@@ -6,6 +6,6 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', view('laravel-query-adviser::index'));
 
 
-Route::get('/query/list', function () {
-    return Cache::get(config('laravel-query-adviser.cache.key'), []);
-});
+//Route::get('/query/list', function () {
+//    return Cache::get(config('laravel-query-adviser.cache.key'), []);
+//});
