528a0ddb1431d108327315ae1ffbd83751ffaf39
mbroersen <m.broersen@socialblue.com>
2020-03-10 21:49:27 +0100

    add server information

--- resources/assets/js/components/page-footer.vue
+++ resources/assets/js/components/page-footer.vue
@@ -5,12 +5,27 @@
                 <strong>Laravel Query Adviser</strong> by
                 <a href="https://socialblue.com">Social Blue</a>.
             </p>
+            <sub class="is-small">
+                {{serverInfo}}
+            </sub>
         </div>
     </footer>
 </template>
 
 <script>
-    export default {
+    import Axios from "Axios";
 
+    export default {
+        data() {
+            return {
+                serverInfo: null
+            }
+        },
+
+        created() {
+            Axios.get('/query-adviser/api/query/server-info').then((response) => {
+                this.serverInfo = response.data;
+            });
+        }
     }
 </script>
