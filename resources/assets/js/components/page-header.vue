c670373d9543f29a2f1674d055d62674aff3cb97
m.broersen <m.broersen@socialblue.com>
2023-07-11 21:03:53 +0200

    refactor: remove vue2 app

--- resources/assets/js/components/page-header.vue
+++
@@ -1,9 +0,0 @@
-<template>
-    <section class="hero is-primary">
-        <div class="hero-body">
-            <div class="container">
-               <svg><use xlink:href="#logo" href="#logo"></use> </svg>
-            </div>
-        </div>
-    </section>
-</template>
